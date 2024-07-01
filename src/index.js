ac347b942759e3078f60d0eacc660e5530ec5e16
671223 <149572149+671223@users.noreply.github.com>
2024-07-01 17:32:31 +0000

    Deploying to gh-pages from @ 671223/goit-js-hw-10@976729b95d6a6aa5401962f30b26fa0556d0ff22 🚀

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { fetchCountries } from './js/fetchCountries.js';
-var _ = require('lodash');
-
-const searchBox = document.getElementById('search-box');
-const debounced = _.debounce(fetchCountries, 300)
-searchBox.addEventListener('input', (event) => {
-    debounced(event.target.value)
-})
